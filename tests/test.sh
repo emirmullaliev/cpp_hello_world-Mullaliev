d214ac910eb84ebed6c3889b6f88a3f584c5296f
emirmullaliev <134895127+emirmullaliev@users.noreply.github.com>
2023-07-06 11:36:59 +0300

    Create test.sh

---
+++ tests/test.sh
@@ -0,0 +1,13 @@
+#1/bin/bash
+
+output=$(./app)
+
+expected_output="06.07.2023"
+
+if [ "$output" == "$expected_output" ]; then
+        echo "Дата вepна"
+        exit 0
+else
+        echo "Дата неверна"
+        exit 0
+fi

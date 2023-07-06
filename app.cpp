8099bd007786748c2a090552b5eae5cd2e729116
emirmullaliev <134895127+emirmullaliev@users.noreply.github.com>
2023-07-06 11:18:06 +0300

    Update app.cpp

--- app.cpp
+++ app.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
  
 int main(){
-    std::cout << "Hello World" << std::endl;
+    std::time_t t = std::time(nullptr);
+    std::tm tm = *std::localtime(&t);
+    std::cout << std::put_time(&tm, "%d.%m.%Y") << '\n';
 }
